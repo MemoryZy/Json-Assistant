9919f40e285f5e9aa027bdab7525c333ebb1002b
Memory <64096987+MemoryZy@users.noreply.github.com>
2024-07-08 01:42:39 +0000

    update与New-Java同步

--- src/main/java/cn/memoryzy/json/actions/JsonToJavaBeanAction.java
+++ src/main/java/cn/memoryzy/json/actions/JsonToJavaBeanAction.java
@@ -49,31 +49,7 @@ public class JsonToJavaBeanAction extends AnAction {
 
     @Override
     public void update(@NotNull AnActionEvent e) {
-        boolean enable = false;
-        // 当前工程
-        Project project = e.getProject();
-        if (Objects.nonNull(project)) {
-            // 鼠标右键选择的路径
-            IdeView ideView = null;
-            try {
-                ideView = e.getRequiredData(LangDataKeys.IDE_VIEW);
-            } catch (Throwable ex) {
-                LOG.debug("## 鼠标右键: " + ex.getMessage() + "\n" + Arrays.toString(ex.getStackTrace()));
-            }
-
-            if (Objects.nonNull(ideView) && ideView.getDirectories().length > 0) {
-                // 文件夹(包)
-                PsiDirectory directory = ideView.getOrChooseDirectory();
-                if (Objects.nonNull(directory)) {
-                    VirtualFile virtualFile = directory.getVirtualFile();
-                    String path = virtualFile.getPath();
-                    enable = path.contains("/test/java") || path.contains("/main/java");
-                }
-            }
-        }
-
-        // 设置可见性
-        e.getPresentation().setVisible(enable);
+        ActionManager.getInstance().getAction("NewClass").update(e);
     }
 
 }
