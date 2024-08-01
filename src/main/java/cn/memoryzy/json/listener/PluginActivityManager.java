6559af50d2b3366d6d3169a124eb68c713d939c4
Memory <1448208262@qq.com>
2024-08-01 20:03:26 +0800

    插件unload后去除标记

--- src/main/java/cn/memoryzy/json/listener/PluginActivityManager.java
+++ src/main/java/cn/memoryzy/json/listener/PluginActivityManager.java
@@ -3,6 +3,8 @@ package cn.memoryzy.json.listener;
 import cn.memoryzy.json.constant.HyperLinks;
 import cn.memoryzy.json.constant.JsonAssistantPlugin;
 import cn.memoryzy.json.utils.Notifications;
+import com.intellij.ide.plugins.DynamicPluginListener;
+import com.intellij.ide.plugins.IdeaPluginDescriptor;
 import com.intellij.ide.util.PropertiesComponent;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupActivity;
@@ -12,8 +14,13 @@ import org.jetbrains.annotations.NotNull;
  * @author Memory
  * @since 2024/7/25
  */
-public class PluginWelcomeAndUpdateManager implements StartupActivity {
+public class PluginActivityManager implements StartupActivity, DynamicPluginListener {
 
+    /**
+     * 项目打开，并在索引建立完后执行
+     *
+     * @param project 项目对象
+     */
     @Override
     public void runActivity(@NotNull Project project) {
         // 验证地址可达性
@@ -36,4 +43,17 @@ public class PluginWelcomeAndUpdateManager implements StartupActivity {
         }
     }
 
+    /**
+     * 插件 Unload 前执行（uninstall 不执行）
+     *
+     * @param pluginDescriptor 插件详情
+     * @param isUpdate         如果插件作为更新安装的一部分被卸载，并且之后将加载新版本，则为true，反之为false
+     */
+    @Override
+    public void beforePluginUnload(@NotNull IdeaPluginDescriptor pluginDescriptor, boolean isUpdate) {
+        if (!isUpdate) {
+            PropertiesComponent propertiesComponent = PropertiesComponent.getInstance();
+            propertiesComponent.unsetValue(JsonAssistantPlugin.PLUGIN_VERSION);
+        }
+    }
 }
