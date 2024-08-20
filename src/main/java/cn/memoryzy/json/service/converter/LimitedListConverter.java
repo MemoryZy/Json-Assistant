0b753a54d1225d13a84092e4682ac0636653b2fc
Memory <1448208262@qq.com>
2024-08-20 21:02:46 +0800

    解决Json历史记录排序问题

--- src/main/java/cn/memoryzy/json/service/converter/LimitedListConverter.java
+++ src/main/java/cn/memoryzy/json/service/converter/LimitedListConverter.java
@@ -6,15 +6,20 @@ import cn.hutool.json.JSONArray;
 import cn.hutool.json.JSONUtil;
 import cn.memoryzy.json.models.LimitedList;
 import cn.memoryzy.json.service.JsonViewerHistoryState;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.util.xmlb.Converter;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Collections;
 
 /**
  * @author Memory
  * @since 2024/8/15
  */
 public class LimitedListConverter extends Converter<LimitedList<String>> {
+
+    private static final Logger LOG = Logger.getInstance(LimitedListConverter.class);
 
     @Override
     public @Nullable LimitedList<String> fromString(@NotNull String value) {
@@ -25,10 +30,14 @@ public class LimitedListConverter extends Converter<LimitedList<String>> {
 
         try {
             JSONArray jsonArray = JSONUtil.parseArray(value);
+            // 应该反着来添加，因 List以 新-旧 向后排，所以应该保持这个顺序
+            // 这里后添加的元素会顶着前面的元素往后，所以先反转 List
+            Collections.reverse(jsonArray);
             for (Object object : jsonArray) {
                 limitedList.add(object.toString());
             }
-        } catch (Exception ignored) {
+        } catch (Exception ex) {
+            LOG.error(ex.getMessage(), ex);
         }
 
         return limitedList;
