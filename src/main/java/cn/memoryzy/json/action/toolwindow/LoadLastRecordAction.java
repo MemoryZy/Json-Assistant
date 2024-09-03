8de09491bd7f82799bae9a62591c532b20f50a90
Memory <1448208262@qq.com>
2024-09-03 18:21:43 +0800

    去除冗余

--- src/main/java/cn/memoryzy/json/action/toolwindow/LoadLastRecordAction.java
+++ src/main/java/cn/memoryzy/json/action/toolwindow/LoadLastRecordAction.java
@@ -7,30 +7,22 @@ import cn.memoryzy.json.constant.JsonAssistantPlugin;
 import cn.memoryzy.json.model.LimitedList;
 import cn.memoryzy.json.service.JsonViewerHistoryState;
 import cn.memoryzy.json.util.JsonAssistantUtil;
-import com.intellij.ide.HelpTooltip;
 import com.intellij.ide.util.PropertiesComponent;
-import com.intellij.openapi.actionSystem.ActionToolbar;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.Presentation;
 import com.intellij.openapi.actionSystem.ToggleAction;
-import com.intellij.openapi.actionSystem.ex.CustomComponentAction;
-import com.intellij.openapi.actionSystem.impl.ActionButton;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.registry.Registry;
 import com.intellij.openapi.wm.ex.ToolWindowEx;
 import com.intellij.ui.LanguageTextField;
 import com.intellij.ui.content.Content;
-import com.intellij.util.ui.JBUI;
 import org.jetbrains.annotations.NotNull;
-
-import javax.swing.*;
 
 /**
  * @author Memory
  * @since 2024/9/2
  */
-public class LoadLastRecordAction extends ToggleAction implements CustomComponentAction, DumbAware {
+public class LoadLastRecordAction extends ToggleAction implements DumbAware {
     public static final String IMPORT_RECORD_ENABLED_KEY = JsonAssistantPlugin.PLUGIN_ID_NAME + ".ImportLastRecord";
 
     private final ToolWindowEx toolWindow;
@@ -46,28 +38,6 @@ public class LoadLastRecordAction extends ToggleAction implements CustomComponen
         PropertiesComponent propertiesComponent = PropertiesComponent.getInstance();
         String value = propertiesComponent.getValue(IMPORT_RECORD_ENABLED_KEY);
         if (value == null) propertiesComponent.setValue(IMPORT_RECORD_ENABLED_KEY, Boolean.TRUE.toString());
-    }
-
-    @Override
-    public @NotNull JComponent createCustomComponent(@NotNull Presentation presentation, @NotNull String place) {
-        ActionButton button = new ActionButton(this, presentation, place, ActionToolbar.DEFAULT_MINIMUM_BUTTON_SIZE) {
-            @Override
-            protected void updateToolTipText() {
-                if (Registry.is("ide.helptooltip.enabled")) {
-                    HelpTooltip.dispose(this);
-                    // noinspection DialogTitleCapitalization
-                    new HelpTooltip()
-                            .setTitle(getTemplatePresentation().getText())
-                            .setDescription(JsonAssistantBundle.messageOnSystem("help.tooltip.paste.history.action.description"))
-                            .installOn(this);
-                } else {
-                    setToolTipText(JsonAssistantBundle.messageOnSystem("help.tooltip.paste.history.action.description"));
-                }
-            }
-        };
-
-        button.setBorder(JBUI.Borders.empty(1, 2));
-        return button;
     }
 
     @Override
