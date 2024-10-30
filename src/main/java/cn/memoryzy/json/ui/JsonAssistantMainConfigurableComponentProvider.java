0511aa239c0ba793d9eba374b9ceb8f410474087
Memory <1448208262@qq.com>
2024-10-31 00:31:12 +0800

    增加剪贴板数据转换配置开关

--- src/main/java/cn/memoryzy/json/ui/JsonAssistantMainConfigurableComponentProvider.java
+++ src/main/java/cn/memoryzy/json/ui/JsonAssistantMainConfigurableComponentProvider.java
@@ -4,6 +4,7 @@ import cn.memoryzy.json.bundle.JsonAssistantBundle;
 import cn.memoryzy.json.service.persistent.AttributeSerializationPersistentState;
 import cn.memoryzy.json.service.persistent.EditorOptionsPersistentState;
 import cn.memoryzy.json.ui.dialog.SupportDialog;
+import com.intellij.icons.AllIcons;
 import com.intellij.ui.JBColor;
 import com.intellij.ui.components.ActionLink;
 import com.intellij.ui.components.JBCheckBox;
@@ -45,6 +46,9 @@ public class JsonAssistantMainConfigurableComponentProvider {
     private JBLabel jacksonLabel;
     private JBLabel jacksonDesc;
     private ActionLink donateLink;
+    private JBCheckBox recognizeOtherFormatsCb;
+    private JBLabel recognizeOtherFormatsLabel;
+    private JBLabel recognizeOtherFormatsDesc;
 
     private final EditorOptionsPersistentState editorOptionsPersistentState = EditorOptionsPersistentState.getInstance();
     private final AttributeSerializationPersistentState attributeSerializationPersistentState = AttributeSerializationPersistentState.getInstance();
@@ -67,7 +71,7 @@ public class JsonAssistantMainConfigurableComponentProvider {
         // ---------------- 属性序列化
         attributeSerializationLabel.setText(JsonAssistantBundle.messageOnSystem("setting.component.attribute.serialization.text"));
         // ---------- 包含随机值
-        includeRandomValuesLabel.setText(JsonAssistantBundle.messageOnSystem("setting.component.random.value.label.text"));
+        includeRandomValuesLabel.setText(JsonAssistantBundle.messageOnSystem("setting.component.random.value.text"));
         includeRandomValuesCb.setSelected(attributeSerializationPersistentState.includeRandomValues);
 
         includeRandomValuesDesc.setCopyable(true);
@@ -76,7 +80,7 @@ public class JsonAssistantMainConfigurableComponentProvider {
         includeRandomValuesDesc.setText(JsonAssistantBundle.messageOnSystem("setting.component.random.value.desc"));
 
         // ---------- 识别 FastJson 注解
-        fastJsonLabel.setText(JsonAssistantBundle.messageOnSystem("setting.component.fastjson.label.text"));
+        fastJsonLabel.setText(JsonAssistantBundle.messageOnSystem("setting.component.fastjson.text"));
         fastJsonCb.setSelected(attributeSerializationPersistentState.recognitionFastJsonAnnotation);
 
         fastJsonDesc.setCopyable(true);
@@ -84,7 +88,7 @@ public class JsonAssistantMainConfigurableComponentProvider {
         fastJsonDesc.setFont(JBUI.Fonts.smallFont());
         fastJsonDesc.setText(JsonAssistantBundle.messageOnSystem("setting.component.fastjson.desc"));
 
-        jacksonLabel.setText(JsonAssistantBundle.messageOnSystem("setting.component.jackson.label.text"));
+        jacksonLabel.setText(JsonAssistantBundle.messageOnSystem("setting.component.jackson.text"));
         jacksonCb.setSelected(attributeSerializationPersistentState.recognitionJacksonAnnotation);
 
         jacksonDesc.setCopyable(true);
@@ -100,24 +104,36 @@ public class JsonAssistantMainConfigurableComponentProvider {
         toolWindowLabel.setText(JsonAssistantBundle.messageOnSystem("setting.component.tool.window.text"));
         // ---------- 导入最新 JSON 记录
         loadLastRecordCb.setSelected(editorOptionsPersistentState.loadLastRecord);
-        loadLastRecordLabel.setText(JsonAssistantBundle.messageOnSystem("editor.options.configurable.component.load.last.record.cb.text"));
+        loadLastRecordLabel.setText(JsonAssistantBundle.messageOnSystem("setting.component.load.last.record.text"));
 
         loadLastRecordDesc.setCopyable(true);
         loadLastRecordDesc.setForeground(JBColor.GRAY);
         loadLastRecordDesc.setFont(JBUI.Fonts.smallFont());
-        loadLastRecordDesc.setText(JsonAssistantBundle.messageOnSystem("editor.options.configurable.component.load.last.record.cb.tip.text"));
+        loadLastRecordDesc.setText(JsonAssistantBundle.messageOnSystem("setting.component.load.last.record.desc"));
 
         // ---------- 跟随主编辑器配色主题
         followEditorThemeCb.setSelected(editorOptionsPersistentState.followEditorTheme);
-        followEditorThemeLabel.setText(JsonAssistantBundle.messageOnSystem("editor.options.configurable.component.follow.editor.theme.cb.text"));
+        followEditorThemeLabel.setText(JsonAssistantBundle.messageOnSystem("setting.component.follow.editor.theme.text"));
 
         // ---------- 展示编辑器行号
         displayLineNumbersCb.setSelected(editorOptionsPersistentState.displayLineNumbers);
-        displayLineNumbersLabel.setText(JsonAssistantBundle.messageOnSystem("editor.options.configurable.component.display.lines.cb.text"));
+        displayLineNumbersLabel.setText(JsonAssistantBundle.messageOnSystem("setting.component.display.lines.text"));
 
         // ---------- 显示折叠轮廓
         foldingOutlineCb.setSelected(editorOptionsPersistentState.foldingOutline);
-        foldingOutlineLabel.setText(JsonAssistantBundle.messageOnSystem("editor.options.configurable.component.folding.outline.cb.text"));
+        foldingOutlineLabel.setText(JsonAssistantBundle.messageOnSystem("setting.component.folding.outline.text"));
+
+        recognizeOtherFormatsCb.setSelected(false);
+        recognizeOtherFormatsLabel.setText(JsonAssistantBundle.messageOnSystem("setting.component.recognize.other.formats.text"));
+
+        recognizeOtherFormatsDesc.setCopyable(true);
+        recognizeOtherFormatsDesc.setForeground(JBColor.GRAY);
+        recognizeOtherFormatsDesc.setFont(JBUI.Fonts.smallFont());
+        recognizeOtherFormatsDesc.setText(JsonAssistantBundle.messageOnSystem("setting.component.recognize.other.formats.desc"));
+
+        recognizeOtherFormatsDesc.setIcon(AllIcons.General.ContextHelp);
+        recognizeOtherFormatsLabel.setVerticalTextPosition(SwingConstants.CENTER);
+        recognizeOtherFormatsLabel.setHorizontalTextPosition(SwingConstants.RIGHT);
 
         addToolWindowComponentClickListener();
     }
