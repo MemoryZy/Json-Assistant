fe9417b44cf0370fab5e2140a3a1af6cb664bf8b
Memory <1448208262@qq.com>
2025-07-03 14:24:08 +0800

    增加历史记录触发事件

---
+++ src/main/java/cn/memoryzy/json/ui/listener/AddHistoryAction.java
@@ -0,0 +1,18 @@
+package cn.memoryzy.json.ui.listener;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+/**
+ * @author Memory
+ * @since 2025/7/3
+ */
+public class AddHistoryAction extends AbstractAction {
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
