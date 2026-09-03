02ba29acc90cc6d026d15ece03d0ce27b19e6688
Memory <memoryzk@outlook.com>
2026-09-03 16:49:04 +0800

    支持直接唤醒 JSON 比对窗口

---
+++ src/main/java/cn/memoryzy/json/action/JsonTextDiffAnywhereAction.java
@@ -0,0 +1,60 @@
+package cn.memoryzy.json.action;
+
+import cn.memoryzy.json.bundle.JsonAssistantBundle;
+import cn.memoryzy.json.constant.FileTypeHolder;
+import cn.memoryzy.json.util.JsonAssistantUtil;
+import com.intellij.diff.DiffContentFactory;
+import com.intellij.diff.contents.DocumentContent;
+import com.intellij.diff.editor.DiffVirtualFile;
+import com.intellij.diff.editor.SimpleDiffVirtualFile;
+import com.intellij.diff.requests.SimpleDiffRequest;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.actionSystem.Presentation;
+import com.intellij.openapi.actionSystem.UpdateInBackground;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.openapi.project.DumbAwareAction;
+import com.intellij.openapi.project.Project;
+import icons.JsonAssistantIcons;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+/**
+ * @author Memory
+ * @since 2026/9/3
+ */
+public class JsonTextDiffAnywhereAction extends DumbAwareAction implements UpdateInBackground {
+
+    public JsonTextDiffAnywhereAction() {
+        super();
+        setEnabledInModalContext(true);
+        Presentation presentation = getTemplatePresentation();
+        presentation.setText(JsonAssistantBundle.message("action.openJsonDiffWindow.text"));
+        presentation.setDescription(JsonAssistantBundle.messageOnSystem("action.openJsonDiffWindow.description"));
+        presentation.setIcon(JsonAssistantIcons.DIFF);
+    }
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent event) {
+        Project project = event.getProject();
+        FileType fileType = FileTypeHolder.JSON5;
+        DiffContentFactory diffContentFactory = DiffContentFactory.getInstance();
+        DocumentContent leftDocumentContent = diffContentFactory.createEditable(project, "", fileType);
+        DocumentContent rightDocumentContent = diffContentFactory.createEditable(project, "", fileType);
+
+        SimpleDiffRequest simpleDiffRequest = new SimpleDiffRequest(
+                JsonAssistantBundle.messageOnSystem("dialog.json.text.diff.main.title"),
+                leftDocumentContent,
+                rightDocumentContent,
+                JsonAssistantBundle.messageOnSystem("dialog.json.text.diff.left.title"),
+                JsonAssistantBundle.messageOnSystem("dialog.json.text.diff.right.title"));
+
+        DiffVirtualFile file = new SimpleDiffVirtualFile(simpleDiffRequest);
+        file.putUserData(JsonTextDiffAction.DIFF_REQUEST_KEY, simpleDiffRequest);
+        FileEditorManager manager = FileEditorManager.getInstance(Objects.requireNonNull(project));
+        JsonAssistantUtil.invokeMethod(manager, "openFileInNewWindow", file);
+    }
+
+}
